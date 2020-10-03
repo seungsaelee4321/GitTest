efcdbaab108d0504665ca326d5eb334c709756ea
seungsaelee4321 <sslee4710@gmail.com>
2020-10-03 11:09:27 +0900

    버튼 삭제

--- app/src/main/java/com/sslee4321/gittest/MainActivity.kt
+++ app/src/main/java/com/sslee4321/gittest/MainActivity.kt
@@ -2,15 +2,10 @@ package com.sslee4321.gittest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        btn_test2.text = "test..."
     }
 }
