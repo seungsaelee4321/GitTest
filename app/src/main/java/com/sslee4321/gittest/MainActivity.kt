696a8af2ea24a54cf2e1ac8be873d8c26f7b2f59
seungsaelee4321 <sslee4710@gmail.com>
2020-10-03 13:19:20 +0900

    test button add

--- app/src/main/java/com/sslee4321/gittest/MainActivity.kt
+++ app/src/main/java/com/sslee4321/gittest/MainActivity.kt
@@ -2,10 +2,17 @@ package com.sslee4321.gittest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_test.setOnClickListener {
+            Toast.makeText(this@MainActivity, "test",
+                Toast.LENGTH_SHORT).show()
+        }
     }
 }
